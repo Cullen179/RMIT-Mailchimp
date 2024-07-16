a9f35aba7067b3f694383a24e9ed85db0ed7ad3c
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-07-16 16:55:44 +0700

    Update action.ts

--- app/action.ts
+++ app/action.ts
@@ -1,6 +1,5 @@
 'use server';
-
-import * as XLSX from "xlsx";
+import { Member } from "./page";
 export async function getMailchimp(url: string, apiKey: string) {
   const mailchimpData = await fetch(url, {
     method: "GET",
@@ -10,8 +9,29 @@ export async function getMailchimp(url: string, apiKey: string) {
   })
     .then((res) => res.json())
     .then((data) => {
-      return data;
+        return data;
     });
 
   return mailchimpData;
 }
+
+export async function getMembers(url: string, apiKey: string) {
+  const members = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `${apiKey}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+        return {
+            listID: data.list_id,
+            memberID: data.id,
+            memberEmail: data.email_address,
+      } as Member;
+    });
+
+  return members;
+}
+
+
